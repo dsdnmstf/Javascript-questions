6cb2e50397c354f49d2b8a9027e8a5d0befa868d
musrafa <dsdnmstf@gmail.com>
2022-02-04 23:02:19 +0100

    first

---
+++ answers.js
@@ -0,0 +1,15 @@
+// !First   Question
+// let total = 0
+// let number = +prompt('Please enterr a number')
+// for (  i = 1; i < number; i++) {
+//     if( number % i == 0){
+//         total += i
+//     }
+    
+// }
+// if ((number == total)) {
+//   console.log(`${number} is a perfect number`);
+// } else {
+//     console.log(`${number} is not a perfect number`);
+// }
+    
